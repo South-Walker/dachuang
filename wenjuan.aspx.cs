cf25de0adf456a4d09c441f458f818aa84359c90
Nanxing Xiao <506830915@qq.com>
2017-03-02 12:21:37 +0800

    last version before the big change

--- wenjuan.aspx.cs
+++ wenjuan.aspx.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Linq.Expressions;
+using System.Data;
+using System.Data.Linq;
 using System.Data.SqlClient;
 
 public partial class wenjuan : System.Web.UI.Page
@@ -62,5 +61,51 @@ public partial class wenjuan : System.Web.UI.Page
                 Response.Write("<script>alert('第" + i.ToString() + "题怕是没写完');</script>");
             }
         }
+        LinkDBDataContext lqdb = new LinkDBDataContext();
+        var que = new dachuang
+        {
+            grade = allanswer[0],
+            major = allanswer[1],
+            sex = allanswer[2],
+            q1 = Convert.ToInt32(allanswer[3]),
+            q2 = Convert.ToInt32(allanswer[4]),
+            q3 = Convert.ToInt32(allanswer[5]),
+            q4 = Convert.ToInt32(allanswer[6]),
+            q5 = Convert.ToInt32(allanswer[7]),
+            q6 = Convert.ToInt32(allanswer[8]),
+            q7 = Convert.ToInt32(allanswer[9]),
+            q8 = Convert.ToInt32(allanswer[10]),
+            q9 = Convert.ToInt32(allanswer[11]),
+            q10 = Convert.ToInt32(allanswer[12]),
+            q11 = Convert.ToInt32(allanswer[13]),
+            q12 = Convert.ToInt32(allanswer[14]),
+            q13 = Convert.ToInt32(allanswer[15]),
+            q14 = Convert.ToInt32(allanswer[16]),
+            q15 = Convert.ToInt32(allanswer[17]),
+            q16 = Convert.ToInt32(allanswer[18]),
+            q17 = Convert.ToInt32(allanswer[19]),
+            q18 = Convert.ToInt32(allanswer[20]),
+            q19 = Convert.ToInt32(allanswer[21]),
+            q20 = Convert.ToInt32(allanswer[22]),
+            q21 = Convert.ToInt32(allanswer[23]),
+            q22 = Convert.ToInt32(allanswer[24]),
+            q23 = allanswer[25],
+            q24 = Convert.ToInt32(allanswer[26]),
+            q25 = Convert.ToInt32(allanswer[27]),
+            q26 = Convert.ToInt32(allanswer[28]),
+            q27 = Convert.ToInt32(allanswer[29]),
+            q28 = Convert.ToInt32(allanswer[30]),
+            q29 = Convert.ToInt32(allanswer[31]),
+            q30 = Convert.ToInt32(allanswer[32]),
+            q31 = Convert.ToInt32(allanswer[33]),
+            q32 = Convert.ToInt32(allanswer[34]),
+            q33 = Convert.ToInt32(allanswer[35]),
+            q34 = Convert.ToInt32(allanswer[36]),
+            q35 = Convert.ToInt32(allanswer[37])
+        };
+        lqdb.dachuang.InsertOnSubmit(que);
+        lqdb.SubmitChanges();
+        var q = lqdb.dachuang.Max(c=>c.mainID);
+        Response.Write("<script>alert('第" + q.ToString() + "张完成！');</script>");
     }
 }
